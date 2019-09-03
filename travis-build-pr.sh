95e3cd9af887ac060ba53785841177f3ae4d6afd
Nathan Zender <github@nathanzender.com>
2019-09-02 21:13:04 -0400

    Rollback extra debugging put in place

--- travis-build-pr.sh
+++ travis-build-pr.sh
@@ -4,7 +4,7 @@ EXIT_STATUS=0
 
 ./gradlew --stop
 ./gradlew testClasses --no-daemon
-./gradlew check -i --no-daemon || EXIT_STATUS=$?
+./gradlew check --no-daemon || EXIT_STATUS=$?
 
 
 exit $EXIT_STATUS
