ecb5e51c42bea384a2abdeaac0a3c61187047e30
Nathan Zender <github@nathanzender.com>
2020-04-14 07:15:35 -0400

    Checkstyle stuff

--- acme-cli/src/main/java/io/micronaut/acme/cli/AcmeServerOption.java
+++ acme-cli/src/main/java/io/micronaut/acme/cli/AcmeServerOption.java
@@ -1,8 +1,27 @@
+/*
+ * Copyright 2017-2020 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.micronaut.acme.cli;
 
 import picocli.CommandLine;
 
-public class AcmeServerOption {
+/**
+ * Allows for helper flags to make selecting which ACME environment you are using easier.
+ */
+public final class AcmeServerOption {
 
     public static final String LE_PROD_URL = "https://acme-v02.api.letsencrypt.org/directory";
 
@@ -17,7 +36,10 @@ public class AcmeServerOption {
     @CommandLine.Option(names = {"--lets-encrypt-staging"}, required = true, description = "Use the Let's Encrypt prod URL")
     private boolean letsEncryptStaging;
 
-
+    /**
+     * Obtains the configured server url.
+     * @return ACME server url
+     */
     public String serverUrl() {
         if (letsEncryptProd) {
             return LE_PROD_URL;
